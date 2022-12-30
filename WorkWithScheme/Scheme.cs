8f0a1221dacf7f075317bdf6374f8dfdf2b6bbb0
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2022-12-30 10:32:42 +0500

    Added WorkWithScheme

---
+++ WorkWithScheme/Scheme.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DummyDB.WorkWithScheme
+{
+    public class Scheme
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; }
+
+        [JsonProperty(PropertyName = "columns")]
+        public List<SchemeColumn> Columns { get; }
+    }
+}
