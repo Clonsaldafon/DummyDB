a81418279f2fb717edd9b77d3bd26bf416b985c6
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2022-12-30 09:34:57 +0500

    Added constructors

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DummyDB
 {
@@ -6,7 +7,7 @@ namespace DummyDB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
--- Table.cs
+++ Table.cs
@@ -2,15 +2,38 @@
 
 namespace DummyDB
 {
+    public class TableScheme
+    {
+        public string Name { get; }
+        public List<Column> Columns { get; }
+
+        public TableScheme(string name, List<Column> columns)
+        {
+            Name = name;
+            Columns = columns;
+        }
+    }
+
     public class Table
     {
         public List<Row> Rows { get; }
         private TableScheme TableScheme { get; }
-}
+
+        public Table(List<Row> rows, TableScheme tableScheme)
+        {
+            Rows = rows;
+            TableScheme = tableScheme;
+        }
+    }
 
     public class Row
     {
         public Dictionary<Column, object> Data { get; }
+
+        public Row(Dictionary<Column, object> data)
+        {
+            Data = data;
+        }
     }
 
     public class Column
@@ -18,6 +41,11 @@ namespace DummyDB
         public string Name { get; }
 
         public ColumnType DataType { get; }
+
+        public Column(string name)
+        {
+            Name = name;
+        }
     }
 
     public enum ColumnType
