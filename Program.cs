44506ab2401c68a66d10c36954db76d4e7d8adc5
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-02-02 18:20:05 +0500

    v0.1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace DummyDB
 {
@@ -7,7 +9,33 @@ namespace DummyDB
     {
         static void Main(string[] args)
         {
-            
+            string projectPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+            string schemesPath = string.Concat(projectPath, @"\JSONScheme\TestScheme\");
+            string dataPath = string.Concat(projectPath, @"\TestData\");
+
+            string schemePath1 = string.Concat(schemesPath, "Clients-scheme.json");
+            string schemePath2 = string.Concat(schemesPath, "Products-scheme.json");
+
+            string dataPath1 = string.Concat(dataPath, "Clients-data.csv");
+            string dataPath2 = string.Concat(dataPath, "Products-data.csv");
+
+            TableScheme tableScheme1 = JsonConvert.DeserializeObject<TableScheme>(File.ReadAllText(schemePath1));
+            TableScheme tableScheme2 = JsonConvert.DeserializeObject<TableScheme>(File.ReadAllText(schemePath2));
+
+            Table table1 = new Table(new List<Row>(), tableScheme1);
+            Table table2 = new Table(new List<Row>(), tableScheme2);
+
+            string[] rows1 = GetRowsFromCSV(dataPath1);
+            for (int i = 1; i < rows1.Length; i++)
+            {
+                
+            }
+        }
+
+        static string[] GetRowsFromCSV(string dataPath)
+        {
+            return File.ReadAllText(dataPath).Split("\n");
         }
     }
 }
